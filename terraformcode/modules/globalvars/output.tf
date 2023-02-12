6128b4b98deb46a68a3410c7aa2bf106f932ce06
Manish Bhatia <servicedly@hotmail.com>
2023-02-12 04:56:48 +0000

    TF Code and other files

---
+++ terraformcode/modules/globalvars/output.tf
@@ -0,0 +1,13 @@
+# Default tags
+output "default_tags" {
+  value = {
+    "Owner" = "Manish"
+    "App"   = "Web"
+    "Project" = "CLOassignment"
+  }
+}
+
+# Prefix to identify resources
+output "prefix" {
+  value     = "assignment1"
+}
