b51e2836e321cc349187a47e1d797362327837eb
Manish Bhatia <servicedly@hotmail.com>
2023-02-13 00:17:26 +0000

    Added user data file

---
+++ terraformcode/dev/instances/user_data.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo service docker start
+export ECR=570629616813.dkr.ecr.us-east-1.amazonaws.com
+export DBECR=$ECR/database-image-clo835-docker-assignment:latest
+export APPECR=$ECR/app-image-clo835-docker-assignment:latest
+aws ecr get-login-password --region us-east-1 |sudo docker login -u AWS ${ECR} --password-stdin   
+sudo docker network create servicedly
+sudo docker run --name mysql-db --network=customBridge -d -e MYSQL_ROOT_PASSWORD=pw $DBECR
+sleep 40
+sudo docker run -d --name blue -p 8081:8080  --servicedly -e APP_COLOR=$APP_COLOR -e DBHOST=$DBHOST -e DBPORT=$DBPORT -e  DBUSER=$DBUSER -e DBPWD=$DBPWD $APPECR
+sudo docker run -d --name lime -p 8082:8080  --servicedly -e APP_COLOR=$APP_COLOR -e DBHOST=$DBHOST -e DBPORT=$DBPORT -e  DBUSER=$DBUSER -e DBPWD=$DBPWD $APPECR
+sudo docker run -d --name pink -p 8083:8080  --servicedly -e APP_COLOR=$APP_COLOR -e DBHOST=$DBHOST -e DBPORT=$DBPORT -e  DBUSER=$DBUSER -e DBPWD=$DBPWD $APPECR
